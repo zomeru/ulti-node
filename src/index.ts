8982c2c847629bd8c454409f973f15962f81a01d
zomeru <zomerzxc.19@gmail.com>
2022-12-22 02:56:44 +0800

    Query db, seed data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,49 @@
 import express from 'express';
 import morgan from 'morgan';
+import { PrismaClient } from '@prisma/client';
+import { nanoid } from 'nanoid';
+
+const genId = () => nanoid(16);
+
+const db = new PrismaClient({
+  log: ['error', 'query', 'info', 'warn'],
+});
+
+const seedDatabase = async () => {
+  console.log('Seeding database...');
+
+  const postsCount = await db.post.count();
+
+  if (postsCount === 0) {
+    await db.post.createMany({
+      data: [
+        {
+          id: genId(),
+          slug: 'ulti-node',
+          title: 'ulti-node',
+          content: 'ulti-node',
+          publishedAt: new Date(),
+        },
+        {
+          id: genId(),
+          slug: 'ulti-react',
+          title: 'ulti-react',
+        },
+      ],
+    });
+    console.log('Database seeded.');
+  }
+};
+seedDatabase();
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
+app.get('/', async (req, res) => {
+  const posts = await db.post.findMany();
+
+  res.json(posts);
 });
 
 const port = Number(process.env.PORT) || 8080;
