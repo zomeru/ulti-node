abdf071668e70fae317334802ae0a26bea3605aa
zomeru <zomerzxc.19@gmail.com>
2022-12-21 23:31:15 +0800

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+const port = Number(process.env.PORT) || 8080;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}`);
+});
